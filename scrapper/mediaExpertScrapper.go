11e6948f8c2c5cc77b4e6f52c5b51251dced800d
rustoma <rutoma00@gmail.com>
2023-05-20 07:52:39 +0200

    feat: fix scrapper when waiting for products description and add getProductImage fn

--- scrapper/mediaExpertScrapper.go
+++ scrapper/mediaExpertScrapper.go
@@ -1,21 +1,25 @@
 package scrapper
 
 import (
+	"encoding/base64"
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
+	"io"
 	"log"
+	"net/http"
 	"regexp"
 	"strings"
 )
 
 type Result struct {
-	name string
-	url  string
+	Name string `json:"name"`
+	Url  string `json:"url"`
 }
 
 type Feature struct {
-	name  string
-	value string
+	Name  string `json:"name"`
+	Value string `json:"value"`
 }
 
 type MediaExpertScrapper struct {
@@ -23,10 +27,12 @@ type MediaExpertScrapper struct {
 }
 
 type Product struct {
-	Name        string
-	Description string
-	Features    []Feature
-	Url         string
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Features    []Feature `json:"features"`
+	Url         string    `json:"url"`
+	MainImage   string    `json:"main_image"`
+	OrderWeight int       `json:"order_weight"`
 }
 
 func (mediaScrapper *MediaExpertScrapper) Scrap(domain string, url string, categories ...string) []Product {
@@ -60,27 +66,35 @@ func (mediaScrapper *MediaExpertScrapper) Scrap(domain string, url string, categ
 
 	for _, product := range products {
 
-		log.Printf("Waiting for body tag on %s product page...", product.name)
+		//var body string
+
+		log.Printf("Waiting for body tag on %s product page...", product.Name)
 		// Send HTTP GET request to website URL
 		err = chromedp.Run(mediaScrapper.config.ctx,
-			chromedp.Navigate(product.url),
+			chromedp.Navigate(product.Url),
 			chromedp.Sleep(mediaScrapper.config.delay),
 			chromedp.WaitVisible("body", chromedp.ByQuery),
+			//chromedp.OuterHTML("body", &body, chromedp.ByQuery),
 		)
 		log.Println("Body tag was found.")
+
+		//log.Println(body)
+
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		var table string
 		var description string
-		log.Printf("Waiting for specification table and content on %s product page...", product.name)
+		var image string
+		log.Printf("Waiting for specification table and content on %s product page...", product.Name)
 		err = chromedp.Run(mediaScrapper.config.ctx,
-			chromedp.WaitVisible(".content > .description", chromedp.ByQuery),
+			chromedp.WaitVisible(".item.description", chromedp.ByQuery),
 			chromedp.OuterHTML(".specification > table", &table, chromedp.ByQuery),
-			chromedp.OuterHTML(".content > .description", &description, chromedp.ByQuery),
+			chromedp.OuterHTML(".item.description", &description, chromedp.ByQuery),
+			chromedp.OuterHTML(".gallery-slide-image", &image, chromedp.ByQuery),
 		)
-		log.Printf("Specification table and content was found on %s product page.", product.name)
+		log.Printf("Specification table and content was found on %s product page.", product.Name)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -99,11 +113,19 @@ func (mediaScrapper *MediaExpertScrapper) Scrap(domain string, url string, categ
 
 		productDescription := mediaScrapper.getDescription(doc)
 
+		doc, err = goquery.NewDocumentFromReader(strings.NewReader(image))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		productImage := mediaScrapper.getProductImage(doc)
+
 		scrapingResult := Product{
-			Name:        product.name,
+			Name:        product.Name,
 			Description: productDescription,
 			Features:    features,
-			Url:         product.url,
+			Url:         product.Url,
+			MainImage:   productImage,
 		}
 
 		scrapingResults = append(scrapingResults, scrapingResult)
@@ -145,15 +167,45 @@ func (mediaScrapper *MediaExpertScrapper) getSpecifications(doc *goquery.Documen
 	doc.Find("tbody tr").Each(func(i int, row *goquery.Selection) {
 		feature := Feature{}
 		row.Find("th").Each(func(j int, col *goquery.Selection) {
-			feature.name = strings.TrimSpace(col.Text())
+			feature.Name = strings.TrimSpace(col.Text())
 		})
 		row.Find("td").Each(func(j int, col *goquery.Selection) {
-			feature.value = strings.TrimSpace(col.Text())
+			feature.Value = strings.TrimSpace(col.Text())
 		})
 		features = append(features, feature)
 	})
 
 	return features
+}
+
+func (mediaScrapper *MediaExpertScrapper) getProductImage(doc *goquery.Document) string {
+
+	var imageUrl string
+
+	image := doc.Find(".image-magnifier > .spark-image").First()
+
+	url, ok := image.Attr("src")
+	if ok {
+		imageUrl = url
+	}
+
+	resp, err := http.Get(imageUrl)
+	if err != nil {
+		fmt.Println("Failed to fetch image:", err)
+
+	}
+	defer resp.Body.Close()
+
+	// Read the image data into memory
+	imageData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Failed to read image data:", err)
+	}
+
+	dataURI := "data:image/jpeg;base64," + base64.StdEncoding.EncodeToString(imageData)
+
+	return dataURI
+
 }
 
 func (mediaScrapper *MediaExpertScrapper) getTenFirstResults(domain string, doc *goquery.Document) []Result {
@@ -163,15 +215,15 @@ func (mediaScrapper *MediaExpertScrapper) getTenFirstResults(domain string, doc 
 	doc.Find(".name.is-section").Each(func(i int, row *goquery.Selection) {
 		result := Result{}
 
-		result.name = strings.TrimSpace(row.Text())
+		result.Name = strings.TrimSpace(row.Text())
 
 		row.Find("a").Each(func(i int, row *goquery.Selection) {
 			if i == 0 {
 				href, exists := row.Attr("href")
 				if exists {
-					result.url = domain + href
+					result.Url = domain + href
 				} else {
-					result.url = "#"
+					result.Url = "#"
 				}
 
 			}
@@ -179,5 +231,5 @@ func (mediaScrapper *MediaExpertScrapper) getTenFirstResults(domain string, doc 
 		results = append(results, result)
 	})
 
-	return results[:10]
+	return results[:2]
 }
