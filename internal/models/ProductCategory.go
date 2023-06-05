08a10d49602d082969e3fd23e33fc882b3142839
rustoma <rutoma00@gmail.com>
2023-06-05 21:21:43 +0200

    feat: add ProductCategory model

---
+++ internal/models/ProductCategory.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type ProductCategory struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
